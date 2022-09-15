f405c50e45e27181583191fd1b720c614bd112b1
TanviralamSust <tanviralam@student.sust.edu>
2022-09-15 12:13:15 -0600

    Done homepage basic UI

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import classes from "../styles/Nav.module.css";
+import logo from "../assets/images/logo-bg.png"
+import Account from "./Account";
+
+
+export default function Nav() {
+    return (
+        <nav className={classes.nav}>
+            <ul>
+                <li>
+                    <a className={classes.brand}>
+                        <img src={logo} alt="Learn with Sumit Logo" />
+                        <h3>Learn with Sumit</h3>
+                    </a>
+                </li>
+            </ul>
+            <Account />
+        </nav>
+    )
+}
